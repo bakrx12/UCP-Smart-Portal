f97c92853991a52dec94239a9aacbf9d39b2c845
bakr <140571818+bakrx12@users.noreply.github.com>
2026-09-20 14:09:04 +0500

    Added design for dashboard.tsx

---
+++ assets/design/dashboard.tsx
@@ -0,0 +1,112 @@
+function WidgetAcdcredits() {
+  return (
+    <div className="absolute contents left-[24px] top-[156px]" data-name="widget_acdcredits">
+      <div className="absolute h-[99px] left-[24px] rounded-[18px] top-[156px] w-[183.939px]" style={{ backgroundImage: "linear-gradient(115.0364675035346deg, rgb(30, 30, 30) 54.657%, rgb(40, 40, 40) 100%)" }} />
+      <p className="[word-break:break-word] absolute font-['Inter:Regular',sans-serif] font-normal h-[50px] leading-[normal] left-[52.19px] not-italic text-[22px] text-white top-[190px] w-[153.056px]">cgpa_widget</p>
+    </div>
+  );
+}
+
+function Headline1Academics() {
+  return (
+    <div className="absolute contents left-[24.83px] top-[74px]" data-name="headline1_academics">
+      <p className="[word-break:break-word] absolute font-['Inter:Regular',sans-serif] font-normal leading-[normal] left-[26.83px] not-italic text-[35px] text-white top-[74px] w-[530px]">Academics</p>
+      <div className="absolute bg-[#6c6c6c] h-[5px] right-[24.17px] top-[125px] w-[1458px]" />
+      <div className="absolute bg-[#008cff] h-[8px] left-[24.83px] top-[125px] w-[161px]" />
+    </div>
+  );
+}
+
+function Headline1Academics1() {
+  return (
+    <div className="absolute contents right-[22.17px] top-[919px]" data-name="headline1_academics">
+      <p className="[word-break:break-word] absolute font-['Inter:Regular',sans-serif] font-normal leading-[normal] left-[28.83px] not-italic text-[35px] text-white top-[919px] w-[530px]">Courses</p>
+      <div className="absolute bg-[#6c6c6c] h-[5px] right-[22.17px] top-[970px] w-[1458px]" />
+      <div className="absolute bg-[#008cff] h-[8px] left-[26.83px] top-[970px] w-[161px]" />
+    </div>
+  );
+}
+
+function WidgetAcdCgpa() {
+  return (
+    <div className="absolute contents left-[227.53px] top-[156px]" data-name="widget_acdCGPA">
+      <div className="absolute h-[99px] left-[227.53px] rounded-[18px] top-[156px] w-[183.939px]" style={{ backgroundImage: "linear-gradient(115.0364675035346deg, rgb(30, 30, 30) 54.657%, rgb(40, 40, 40) 100%)" }} />
+      <p className="[word-break:break-word] absolute font-['Inter:Regular',sans-serif] font-normal h-[32px] leading-[normal] left-[248.83px] not-italic text-[22px] text-white top-[190px] w-[220px]">credits_widget</p>
+    </div>
+  );
+}
+
+function WidgetAcdCgpa1() {
+  return (
+    <div className="absolute contents left-[436.83px] top-[156px]" data-name="widget_acdCGPA">
+      <div className="absolute h-[99px] left-[436.83px] rounded-[18px] top-[156px] w-[239.28px]" style={{ backgroundImage: "linear-gradient(121.28336796719154deg, rgb(30, 30, 30) 54.657%, rgb(40, 40, 40) 100%)" }} />
+      <p className="[word-break:break-word] absolute font-['Inter:Regular',sans-serif] font-normal h-[32px] leading-[normal] left-[464.15px] not-italic text-[22px] text-white top-[190px] w-[299.199px]">termprog_widget</p>
+    </div>
+  );
+}
+
+function WidgetAcdCgpa2() {
+  return (
+    <div className="absolute contents left-[703.83px] top-[156px]" data-name="widget_acdCGPA">
+      <div className="absolute h-[99px] left-[703.83px] rounded-[18px] top-[156px] w-[239.28px]" style={{ backgroundImage: "linear-gradient(121.28336796719154deg, rgb(30, 30, 30) 54.657%, rgb(40, 40, 40) 100%)" }} />
+      <p className="[word-break:break-word] absolute font-['Inter:Regular',sans-serif] font-normal h-[32px] leading-[normal] left-[731.15px] not-italic text-[22px] text-white top-[190px] w-[299.199px]">nextClass_widget</p>
+    </div>
+  );
+}
+
+function WidgetAcdCgpa3() {
+  return (
+    <div className="absolute contents left-[1291.83px] top-[156px]" data-name="widget_acdCGPA">
+      <div className="absolute h-[99px] left-[1291.83px] rounded-[18px] top-[156px] w-[200.44px]" style={{ backgroundImage: "linear-gradient(116.97540971809927deg, rgb(30, 30, 30) 54.657%, rgb(40, 40, 40) 100%)" }} />
+      <p className="[word-break:break-word] absolute font-['Inter:Regular',sans-serif] font-normal h-[32px] leading-[normal] left-[1314.72px] not-italic text-[22px] text-white top-[190px] w-[250.633px]">invoices_widget</p>
+    </div>
+  );
+}
+
+function WidgetAcdCgpa4() {
+  return (
+    <div className="absolute contents left-[1072.83px] top-[156px]" data-name="widget_acdCGPA">
+      <div className="absolute h-[99px] left-[1072.83px] rounded-[18px] top-[156px] w-[200.44px]" style={{ backgroundImage: "linear-gradient(116.97540971809927deg, rgb(30, 30, 30) 54.657%, rgb(40, 40, 40) 100%)" }} />
+      <p className="[word-break:break-word] absolute font-['Inter:Regular',sans-serif] font-normal h-[32px] leading-[normal] left-[1095.72px] not-italic text-[22px] text-white top-[190px] w-[250.633px]">attndce_widget</p>
+    </div>
+  );
+}
+
+function CourseCard() {
+  return (
+    <div className="absolute contents left-[41.83px] pointer-events-none top-[1119px]" data-name="CourseCard">
+      <div className="absolute bottom-0 h-[-919px] left-[41.83px] top-[1119px]">
+        <div className="h-[200px] pointer-events-auto rounded-[20px] sticky top-0 w-[473px]" style={{ backgroundImage: "linear-gradient(120.70593988626608deg, rgb(37, 37, 37) 9.4779%, rgb(26, 26, 26) 87.899%)" }} />
+      </div>
+      <div className="absolute bottom-0 h-[-1055px] left-[58.83px] top-[1255px]">
+        <div className="bg-[#008cff] h-[54px] pointer-events-auto rounded-[40px] sticky top-0 w-[243px]" />
+      </div>
+    </div>
+  );
+}
+
+export default function Frame() {
+  return (
+    <div className="relative size-full">
+      <div className="absolute bg-[rgba(48,48,48,0.88)] bottom-0 right-0 rounded-[15px] top-0 w-[1508px]" />
+      <WidgetAcdcredits />
+      <Headline1Academics />
+      <div className="absolute bottom-0 h-[1333px] left-[514.83px] pointer-events-none top-[310px]">
+        <div className="bg-[rgba(217,217,217,0.2)] h-[541px] pointer-events-auto rounded-[10px] sticky top-0 w-[431px]" />
+      </div>
+      <Headline1Academics1 />
+      <div className="absolute bottom-0 h-[1333px] left-[43.83px] pointer-events-none top-[310px]">
+        <div className="bg-[rgba(217,217,217,0.2)] h-[541px] pointer-events-auto rounded-[10px] sticky top-0 w-[368px]" />
+      </div>
+      <div className="absolute bottom-0 h-[1333px] pointer-events-none right-[76.17px] top-[310px]">
+        <div className="bg-[rgba(217,217,217,0.2)] h-[541px] pointer-events-auto rounded-[10px] sticky top-0 w-[431px]" />
+      </div>
+      <WidgetAcdCgpa />
+      <WidgetAcdCgpa1 />
+      <WidgetAcdCgpa2 />
+      <WidgetAcdCgpa3 />
+      <WidgetAcdCgpa4 />
+      <CourseCard />
+    </div>
+  );
+}
